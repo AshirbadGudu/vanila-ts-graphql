5db162d972c1f2bffced62557c956adb8b649892
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-12-12 08:00:18 +0530

    graphql query implemented in vanilla ts

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,22 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+import "./style.css";
+const continentsHTML = document.querySelector("#continents");
+(async () => {
+  try {
+    const response = await fetch("https://countries.trevorblades.com/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query: `{ continents { name, code } }` }),
+    });
+    const result = await response.json();
+    const continents: Array<{ name: string; code: string }> =
+      result.data.continents;
+    continents.forEach((continent) => {
+      const opt = document.createElement("option");
+      opt.value = continent.code;
+      opt.innerText = continent.name;
+      continentsHTML?.append(opt);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
